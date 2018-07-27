03a1194f9438702e1a26076c79499bc8552358b4
sangeetha <sangeetha.sn@inapp.com>
2018-07-27 22:36:05 +0530

    Second day

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.views import login,logout
 urlpatterns=[
     url(r'^$',views.home),
     url(r'^login/$',login,{'template_name': 'accounts/login.html'}),
-    url(r'^logout/$',login,{'template_name': 'accounts/logout.html'})
-
+    url(r'^logout/$',login,{'template_name': 'accounts/logout.html'}),
+    url(r'^register/$',views.register,name='register'),
 ]
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -5,7 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  apath('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -16,12 +16,15 @@ Including another URLconf
 #from django.contrib import admin
 from django.conf.urls import url,include
 from django.contrib import admin
+from tutorial import views
 
 #from django.urls import path
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
+    url(r'^$',views.login_redirect, name='login_redirect'),
     url(r'^admin/',admin.site.urls),
     url(r'^accounts/',include('accounts.urls')),
-
+    url(r'^home$',views.login_redirect, name='login_redirect'),
+    #url(r'^register',views.register ),
 ]
